07d1565dcb58f5a128571f5f63997a3bc7da851a
xhoi700 <10057@holbertonstudents.com>
2025-03-27 12:32:09 +0100

    report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function createReportObject(employeesList) {
+
+    return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
