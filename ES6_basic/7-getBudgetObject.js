c3d1ac309d428c95932f2ebf5af3052dc65d4465
xhoi700 <10057@holbertonstudents.com>
2025-03-27 12:38:06 +0100

    report object

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,5 @@
 /*eslint-disable*/
 export default function getBudgetObject(income, gdp, capita) {
-	const budget = {
-	  income,
-	  gdp,
-	  capita,
-	};
-  
+	const budget = {income,gdp,capita};
 	return budget;
  }
