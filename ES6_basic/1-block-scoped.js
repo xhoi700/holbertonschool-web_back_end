23976994c9cedd02644d488dc97c518fa09c3f7a
xhoi700 <10057@holbertonstudents.com>
2025-03-27 09:50:38 +0100

    added

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,9 +1,7 @@
-export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-    return [task, task2];
-  }
+/*eslint-disable*/
+export default function taskBlock() {
+  var task = false;
+  var task2 = true;
+
+  return [task, task2];
+}
