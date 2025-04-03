98d5471235189bd5008c43994e38f6edf5df623c
xhoi700 <10057@holbertonstudents.com>
2025-04-03 14:00:18 +0200

    added

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,3 @@
+export default function getFullResponseFromAPI() {
+    return new Promise
+}
---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+export default function signUpUser(firstName, lastName) {
+    return Promise.resolve({
+      firstName: firstName,
+      lastName: lastName
+    });
+}
+  
