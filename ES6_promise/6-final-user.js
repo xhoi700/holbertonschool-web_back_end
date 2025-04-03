d0a1012027be2c83c46664f047544c3d9f0a11fb
xhoi700 <10057@holbertonstudents.com>
2025-04-03 14:07:50 +0200

    added

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) => {
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    }));
+  });
+}
