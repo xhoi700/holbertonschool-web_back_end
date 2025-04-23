2ac0484b5f903dcca87264b9b74adcc6e6a19cf2
xhoi700 <10057@holbertonstudents.com>
2025-04-23 11:05:30 +0200

    added

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sums a list of floats"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums a list of floats"""
+    return sum(input_list)
